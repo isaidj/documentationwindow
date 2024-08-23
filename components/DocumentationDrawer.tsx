ae30f3929f4a752aa2ded4c25e28d282540816bf
isaidj <isaidj199@gmail.com>
2024-08-23 17:05:31 -0500

    animation and togle animaiton

--- components/DocumentationDrawer.tsx
+++ components/DocumentationDrawer.tsx
@@ -1,26 +1,13 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
-import {
-  X,
-  Search,
-  MinimizeIcon,
-  MaximizeIcon,
-  Library,
-  PanelLeft,
-  PanelLeftOpen,
-  PanelLeftClose,
-} from "lucide-react";
-import { useResizable } from "../hooks/useResizable";
-import { Resizer } from "./Resizer";
+import React, { useState, useEffect, useCallback } from "react";
+
+import { X, Search, PanelLeftOpen, PanelLeftClose } from "lucide-react";
 import { SkeletonMarkdown } from "./SkeletonMarkdown";
 import MarkdownVisualizer from "./MarkdownVisualizer";
 import axios from "axios";
-
-import MenuDrawer from "./MenuDrawer";
 import { useDebounce } from "use-debounce";
 import { useDocumentationDrawer } from "@/context/DocumentationDrawerContext";
-import RelatedDocumentation from "./RelatedDocumentation";
+import MenuDrawer from "./MenuDrawer";
 
 const DocumentationDrawer: React.FC = () => {
   const { isOpen, setIsOpen, pathname } = useDocumentationDrawer();
@@ -28,51 +15,52 @@ const DocumentationDrawer: React.FC = () => {
   const [searchTermDebounced] = useDebounce(searchTerm, 500);
   const [documentationContent, setDocumentationContent] = useState("");
   const [loading, setLoading] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuWidth = 256;
+  const drawerWidth = 384;
 
-  const {
-    totalWidth,
-    width,
-    isResizing,
-    isMenuOpen,
-    handleMouseDown,
-    toggleMenu,
-  } = useResizable({
-    minWidth: 300,
-    maxWidthPercentage: 0.8,
-    defaultWidth: 384,
-    menuWidth,
-  });
+  const toggleDrawer = useCallback(() => {
+    const newIsOpen = !isOpen;
+    setIsOpen(newIsOpen);
+    window.parent.postMessage({ type: "drawerToggle", isOpen: newIsOpen }, "*");
+  }, [isOpen, setIsOpen]);
+
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen((prev) => !prev);
+  }, []);
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data && event.data.type === "toggleDrawer") {
+        setIsOpen(event.data.isOpen);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, [setIsOpen]);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "F1") {
         event.preventDefault();
-        setIsOpen((prev) => !prev);
-        if (!isOpen) {
-        }
+        toggleDrawer();
       }
     };
 
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [isOpen]);
+  }, [toggleDrawer]);
 
   useEffect(() => {
     if (isOpen) {
       if (!pathname) {
-        setDocumentationContent("## No se ha seleccionado documentacion");
+        setDocumentationContent("## No se ha seleccionado documentación");
         return;
       }
-      fetchOutlineDocumentation().then((data) => {
-        if (data.error) {
-          setDocumentationContent("## No se encontró documentación");
-          return;
-        }
-        setDocumentationContent(data.data);
-      });
+      fetchOutlineDocumentation();
     }
-  }, [pathname, isOpen]);
+  }, [pathname, isOpen, searchTermDebounced]);
 
   const fetchOutlineDocumentation = async () => {
     setLoading(true);
@@ -81,16 +69,20 @@ const DocumentationDrawer: React.FC = () => {
         "http://localhost:3000/api/documentation/search-documents",
         {
           body: {
-            searchTerm,
+            searchTerm: searchTermDebounced,
             pathname,
           },
         }
       );
 
-      return response.data;
+      if (response.data.error) {
+        setDocumentationContent("## No se encontró documentación");
+      } else {
+        setDocumentationContent(response.data.data);
+      }
     } catch (error) {
       console.error("Error loading documentation:", error);
-      return { error: "Error loading documentation" };
+      setDocumentationContent("## Error al cargar la documentación");
     } finally {
       setLoading(false);
     }
@@ -98,98 +90,81 @@ const DocumentationDrawer: React.FC = () => {
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
-    // Implement local search if desired
   };
+
+  if (!isOpen) return null;
 
   return (
     <div
-      className={`fixed z-50 top-0 right-0 h-full bg-white  ${
-        isOpen ? "translate-x-0" : "translate-x-full shadow-2xl"
+      className={`fixed z-50 top-0 right-0 h-full bg-white shadow-lg flex transition-all duration-75 ease-in-out overflow-hidden ${
+        isOpen ? "fade-in" : "fade-out"
       }`}
-      style={{
-        width: `${totalWidth}px`,
-        transition: isResizing
-          ? "none"
-          : "transform 0.3s ease-in-out, width 0.3s ease-in-out",
-      }}
+      style={{ width: `${drawerWidth}px` }}
     >
-      <button
-        onClick={() => setIsOpen((prev) => !prev)}
-        className="absolute z-50 -left-9 top-1/3 w-9 h-auto p-1 bg-blue-600 text-white hover:bg-blue-700 transition-colors rounded-lg rounded-tr-none rounded-br-none shadow-2xl"
+      <MenuDrawer
+        isOpen={isMenuOpen}
+        setIsOpen={setIsMenuOpen}
+        menuWidth={menuWidth}
+      />
+      <div
+        className="flex flex-col h-full transition-transform duration-75 ease-in-out relative"
+        style={{
+          width: `${drawerWidth}px`,
+          // transform: isMenuOpen
+          //   ? `translateX(${menuWidth}px)`
+          //   : "translateX(0)",
+        }}
       >
-        <Library size={30} />
-      </button>
-
-      <Resizer onMouseDown={handleMouseDown} isOpen={isOpen} />
-
-      <div className="flex flex-row h-full relative">
-        <MenuDrawer
-          isOpen={isMenuOpen}
-          setIsOpen={toggleMenu}
-          menuWidth={menuWidth}
-        />
-
-        <div
-          className="flex flex-col h-full z-40 shadow-2xl flex-grow min-w-72"
-          style={{
-            width: `${width}px`,
-            transition: isResizing ? "none" : "width 0.3s ease-in-out",
-          }}
-        >
-          <div className="flex justify-between items-center p-4 bg-blue-600 text-white">
+        <div className="flex justify-between items-center gap-1 p-4 bg-blue-600 text-white">
+          <button
+            onClick={toggleMenu}
+            className="text-white hover:text-blue-200 transition-colors"
+          >
             {isMenuOpen ? (
-              <PanelLeftClose
-                size={24}
-                onClick={toggleMenu}
-                className="cursor-pointer"
-              />
+              <PanelLeftClose size={24} className="cursor-pointer" />
             ) : (
-              <PanelLeftOpen
-                size={24}
-                onClick={toggleMenu}
-                className="cursor-pointer"
+              <PanelLeftOpen size={24} className="cursor-pointer" />
+            )}
+          </button>
+          <h2 className="text-lg font-semibold">Documentación</h2>
+          <div className="w-6"></div>{" "}
+          {/* Espaciador para mantener el título centrado */}
+        </div>
+
+        {/* Botón de cierre reposicionado */}
+        <button
+          onClick={toggleDrawer}
+          className="absolute top-2 right-2 text-white hover:text-blue-200 transition-colors z-10"
+        >
+          <X size={24} />
+        </button>
+
+        <div className="flex-grow flex flex-col overflow-hidden">
+          <div className="p-4">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Buscar en la documentación..."
+                value={searchTerm}
+                onChange={handleSearch}
+                className="w-full pl-10 pr-4 py-2 border border-blue-300 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <Search
+                className="absolute left-3 top-2.5 text-blue-400"
+                size={16}
+              />
+            </div>
+          </div>
+          <div className="flex-grow p-4 overflow-auto">
+            {loading ? (
+              <SkeletonMarkdown />
+            ) : (
+              <MarkdownVisualizer
+                content={documentationContent}
+                search={searchTermDebounced}
               />
             )}
-
-            <h2 className="text-lg font-semibold">Documentación</h2>
-            <div className="flex items-center space-x-2">
-              <button
-                onClick={() => setIsOpen(false)}
-                className="text-white hover:text-blue-200 transition-colors"
-              >
-                <X size={18} />
-              </button>
-            </div>
           </div>
-
-          <div className="flex-grow flex flex-col overflow-hidden">
-            <div className="p-4">
-              <div className="relative">
-                <input
-                  type="text"
-                  placeholder="Buscar en la documentación..."
-                  value={searchTerm}
-                  onChange={handleSearch}
-                  className="w-full pl-10 pr-4 py-2 border border-blue-300 rounded-md text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <Search
-                  className="absolute left-3 top-2.5 text-blue-400"
-                  size={16}
-                />
-              </div>
-            </div>
-            <div className="flex-grow p-4 overflow-auto">
-              {isResizing || loading ? (
-                <SkeletonMarkdown />
-              ) : (
-                <MarkdownVisualizer
-                  content={documentationContent}
-                  search={searchTermDebounced}
-                />
-              )}
-            </div>
-          </div>
-          {/* <RelatedDocumentation idDocument={pathname} /> */}
         </div>
       </div>
     </div>
